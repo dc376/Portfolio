c81aedbbf745ad8743522c3837aa23ddb6b84c88
dc376 <dc376@njit.edu>
2017-12-29 11:40:38 -0500

    add money to user balance

---
+++ deposit.php
@@ -0,0 +1,96 @@
+<<!DOCTYPE html>
+<html>
+<head>
+<title>Kehoe's Bros Betting</title>
+<style>
+body {margin: 0;}
+#css1{
+    background-color: black;
+    background-position: right bottom;
+    background-repeat: repeat;
+    padding: .5px;
+    }
+.topnav{
+    overflow: hidden;
+    background-color:black;
+    }
+.topnav a{
+    float: center;
+    display: inline;
+    color: white;
+/*    text-align: center;*/
+    padding: 5px 10px;
+    text-decoration: none;
+    font-size: 10px;
+    }
+.topnav a:hover{
+    background-color: lightblue;
+    color: black;
+}
+.topnav a.active{
+    background-color: #ee2211;
+    color: red;
+}   
+.box1{
+    position: relative;
+    margin: auto;
+    float: center;
+    width: 400px;
+    border: 2px solid;
+    font-family:"georgia";
+    height: 120px;
+    background-color: black;
+    color: black;
+    text-align: left;
+    padding-left: .5cm;
+    padding-top: .05cm;
+    padding-bottom: .8cm;
+    background-color: black| transparent;
+    background: rgba(225, 225, 225, .59);
+}    
+</style>
+</head>
+
+<style>
+    { margin: 0; padding: 0; }
+    html {
+        background: url('https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/VYJZA0Avg/rack-focus-of-stadium-lights-in-front-of-a-dark-blue-sky-light-stadium-at-night-baseball-and-hockey-there-is-a-rain-or-snow-military-security-lighting-towers-patrol-in-prison_h9xhm5hjle_thumbnail-full01.png') no-repeat center center fixed;
+       /* background-color: white;*/
+        -webkit-background-size: cover;
+        -moz-background-size: cover;
+        -o-background-size: cover;
+        background-size: cover;
+    }
+</style>
+
+<body>
+<div id = "css1">
+<center>
+<h3><font face="georgia" size="5" color="white">Kehoe's Bros Betting</font></h3>
+    <div class="topnav" id="firstTopNav">
+        <a href="pickaSport.php">HOME</a>
+        <a href="deposit.php">DEPOSIT</a>
+        <a href="about.php">ABOUT</a>
+    </div>
+</center>
+</div>
+</body>
+
+<center>
+<br>
+<br>
+<body>
+
+<form action = "depositingmoney.php" method ="get" style="width: 410px; padding-bottom: .8cm; padding-top: .05cm; border: 2px solid;background-color: black| transparent; background: rgba(225, 225, 225, .65);">
+<!--style="width: 400px; padding-bottom: .8cm; padding-top: .05cm; background-color: #000000;"-->
+<h2><font face="georgia" size="3" color="black"><center>Place deposit money below </center></font></h2>
+
+    <label for="amount">Deposit Amount: </label>
+    <input type = "text" name="amount"  placeholder="Enter Amount"/>
+    <br>
+    <br>
+    <center><input type = "submit"></center>
+</div>
+</form>
+</body>
+</html>
